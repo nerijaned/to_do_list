6302254d7cd8807e4c1bad2d71ddbc330c7f9826
nerijaned <nerizzajane.dejesus@gmail.com>
2024-04-09 15:40:57 -0600

    Commit 1

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_list/screens/my_home_page.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'To-Do List',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage()
+    );
+  }
+}
---
+++ lib/screens/my_home_page.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+         backgroundColor: Color.fromARGB(255, 255, 163, 213),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            SizedBox(
+              height: 80,
+              child: Image.asset('assets/rdplogo.png'),
+            ),
+            Text('Daily Planner',
+            style: TextStyle(
+              fontFamily: 'Caveat',
+              fontSize: 32,
+              color: Colors.white,
+            ),
+            ),
+          ],
+        )
+        ),
+        body: Container(
+          color: Colors.grey[200],
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                  height: 300,
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: TableCalendar(
+                      calendarFormat: CalendarFormat.month,
+                      headerVisible: false,
+                      focusedDay: DateTime.now(),
+                      firstDay: DateTime(2023),
+                      lastDay: DateTime(2025),
+                    ),
+                  ),
+                ),
+              ),
+
+          ],
+          ),
+        ),
+    );
+  }
+}
