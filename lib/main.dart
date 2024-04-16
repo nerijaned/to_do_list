18805ed10c114a620fa97c114432acb819d8bf19
nerijaned <nerizzajane.dejesus@gmail.com>
2024-04-16 15:37:19 -0600

    Commit 0416

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_list/screens/my_home_page.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:to_do_list/firebase_options.dart';
 
-void main() {
+void main() async{
+
+  //Ensure Flutter bindings are initialized
+  WidgetsFlutterBinding.ensureInitialized();
+
+  //Initialized Firebase with the current platform's default options
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MainApp());
 }
 
--- lib/screens/my_home_page.dart
+++ lib/screens/my_home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -9,80 +11,262 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  FirebaseFirestore db = FirebaseFirestore.instance;
+
+  final List<String> tasks = <String>[];
+
+  final List<bool> checkboxes = List.generate(8, (index) => false);
+
+  bool isChecked = false;
+
+  /*
+  The TextEditingController class allows us to 
+  grab the input from the TextField() widget
+  This will be used later on to store the value
+  in the database.
+  */
 
   TextEditingController nameController = TextEditingController();
+
+  void addItemToList() async {
+    final String taskName = nameController.text;
+
+    //Add to the Firestore collection
+    await db.collection('tasks').add({
+      'name': taskName,
+      'completed': null,
+      'timestamp': FieldValue.serverTimestamp(),
+    });
+
+    setState(() {
+      tasks.insert(0, taskName);
+      checkboxes.insert(0, false);
+    });
+  }
+
+  Future<void> fetchTasksFromFirestore() async {
+    
+    //get a reference to the 'tasks' collection in Firestore
+    CollectionReference tasksCollection = db.collection('tasks');
+
+    //Fetch the documents (tasks) from the collection
+    QuerySnapshot querySnapshot = await tasksCollection.get();
+
+    //create an empty list to stre the fetched task names
+    List<String> fetchedTasks = [];
+
+
+    //Loop through each doc (task) in the querySnapshot object
+    for(QueryDocumentSnapshot docSnapshot in querySnapshot.docs){
+      
+      //getting the task name from the document's data
+      String taskName = docSnapshot.get('name');
+
+      //getting the completion status of the task
+      bool completed = docSnapshot.get('completed');
+
+      //add the task name to the list of fetched tasks
+      fetchedTasks.add(taskName);
+    }
+
+    //updating the state to reflect the fetched tasks
+    setState(() {
+      tasks.clear();  //clear the existing task list
+      tasks.addAll(fetchedTasks);
+    });
+
+  }
+
+  //asynchronous function to update the completion status of the task in Firestore
+  Future<void> updateTaskCompletionStatus(String taskName, bool completed) async{
+    
+    //get a reference to the 'tasks' collection in Firestore
+    CollectionReference tasksCollection = db.collection('tasks');
+
+    //query Firestore for documents (tasks) with the given task name
+    QuerySnapshot querySnapshot = await tasksCollection.where('name',isEqualTo: taskName).get();
+
+    //if a matching task document is found
+    if(querySnapshot.size > 0){
+      //get a reference to the first matching document
+      DocumentSnapshot documentSnapshot = querySnapshot.docs[0];
+
+      //update the completed field to the new completion status
+      await documentSnapshot.reference.update({'completed': completed});
+    }
+
+    setState(() {
+      //find the index of the task in the task list
+      int taskIndex = tasks.indexWhere((task) => task == taskName);
+
+      //update the corresponding checkbox value in the checkboxes list
+      checkboxes[taskIndex] = completed;
+    });
+
+  }
+
+  //override the initState method of the State class
+  @override
+  void initState(){
+    super.initState();
+
+    //call the function to fetch the task from the database
+    fetchTasksFromFirestore();
+  } 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-         backgroundColor: Color.fromARGB(255, 255, 163, 213),
-         toolbarHeight: 70.0,
+        /*
+            Rows() and Columns() both have the mainAxisAlignment 
+            property we can utilize to space out their child 
+            widgets to our desired format.
+           */
+        backgroundColor: const Color.fromARGB(255, 255, 148, 184),
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             SizedBox(
-              height: 90,
+              /*
+            SizedBox allows us to control the vertical 
+            and horizontal dimensions by manipulating the 
+            height or width property, or both.
+            */
+              height: 70,
               child: Image.asset('assets/rdplogo.png'),
             ),
-            Text('Daily Planner',
-            style: TextStyle(
-              fontFamily: 'Caveat',
-              fontSize: 32,
-              color: Colors.white,
-            ),
+            Text(
+              'Daily Planner',
+              style: TextStyle(
+                fontFamily: 'Caveat',
+                fontSize: 32,
+                color: Colors.white
+              ),
             ),
           ],
-        )
         ),
-        body: Container(
-          color: Colors.grey[200],
-          child: Column(
-            children: <Widget>[
-              Expanded(
-                child: Container(
-                  height: 300,
-                  color: Colors.white,
-                  child: Padding(
-                    padding: const EdgeInsets.all(7.0),
-                    child: TableCalendar(
-                      calendarFormat: CalendarFormat.month,
-                      headerVisible: false,
-                      focusedDay: DateTime.now(),
-                      firstDay: DateTime(2023),
-                      lastDay: DateTime(2025),
+      ),
+      body: Container(
+        color: Colors.grey[200],
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                height: 300,
+                color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child:
+                      /*
+          The TableCalendar() widget below is installed via 
+          "flutter pub get table_calendar" or by adding the package 
+          to the pubspec.yaml file.  We then import it and implement using
+          configuration properties.  You can set a range and a focus day. 
+          The particulars of implementation for any package can be gleaned 
+          from pub.dev: https://pub.dev/packages/table_calendar.
+          */
+                      SingleChildScrollView(
+                        child: TableCalendar(
+                                            calendarFormat: CalendarFormat.month,
+                                            headerVisible: false,
+                                            focusedDay: DateTime.now(),
+                                            firstDay: DateTime(2023),
+                                            lastDay: DateTime(2025),
+                                          ),
+                      ),
+                ),
+              ),
+            ),
+            ListView.builder(
+                shrinkWrap: true,
+                padding: const EdgeInsets.all(4),
+                itemCount: tasks.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return SingleChildScrollView(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: checkboxes[index]
+                            ? Colors.green.withOpacity(0.7)
+                            : Colors.blue.withOpacity(0.7),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Icon(
+                            !checkboxes[index]
+                              ? Icons.manage_history
+                              : Icons.playlist_add_check_circle,
+                            size: 32,
+                          ),SizedBox(width: 18),
+                          Text('${tasks[index]}',
+                            style: checkboxes[index]
+                              ? TextStyle(decoration: TextDecoration.lineThrough, fontSize: 20, color: Colors.black.withOpacity(0.5))
+                              : TextStyle(fontSize: 20)
+                            ),
+                            Checkbox(value: checkboxes[index], onChanged: (newValue){
+                              setState(() {
+                                checkboxes[index] = newValue!;
+                              });
+                            }),
+                            IconButton(onPressed: null, icon: Icon(Icons.delete)),
+                        ],
+                      ),
+                    ),
+                  );
+                }),
+            Padding(
+              padding: const EdgeInsets.only(top: 12, left: 25, right: 25),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      child: TextField(
+                        controller: nameController,
+                        style: TextStyle(fontSize: 18),
+                        maxLength: 20,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.all(16),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          labelText: 'Add To-Do List Item',
+                          labelStyle: TextStyle(
+                            fontSize: 16,
+                            color: Colors.blue,
+                          ),
+                          hintText: 'Enter your task here',
+                          hintStyle:
+                              TextStyle(fontSize: 16, color: Colors.grey),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(color: Colors.blue, width: 2),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
                     ),
                   ),
-                ),
+                  IconButton(
+                    icon: Icon(Icons.clear),
+                    onPressed: null,
+                  ),
+                ],
               ),
-              Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      width: 350,
-                      height: 100,
-                      child: TextField(
-                      maxLength: 20,
-                      controller: nameController,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10)),
-                          labelText: 'Add To-Do List Item',
-                      ),
-                    )
-                    ),
-                  ],
-                ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  addItemToList();
+                },
+                child: Text('Add To-Do Item'),
               ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: ElevatedButton(onPressed: null, child: Text('Submit')),
-          ),
+            )
           ],
-          ),
         ),
+      ),
     );
   }
 }
